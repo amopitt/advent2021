9b919c6c82b9b80fd93f7b7509cef6fe77ff750f
Sean Amoroso <sean.amoroso@connectiverx.com>
2021-12-05 13:23:02 -0500

    boilerplate for day 6

---
+++ day06/day06.go
@@ -0,0 +1,12 @@
+package day06
+
+import (
+	"advent2021/util"
+	"fmt"
+)
+
+func Day6() {
+	fileName := "day06/sample.txt"
+	contents, _ := util.ReadFile(fileName)
+	fmt.Println(contents)
+}
--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
-import "advent2021/day05"
+import "advent2021/day06"
 
 func main() {
 	//day01.Day1()
 	//day02.Day2()
 	// day03.Day3()
 	//day04.Day4()
-	day05.Day5()
+	// day05.Day5()
+	day06.Day6()
 }
