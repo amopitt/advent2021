67e3e6d45e3d7fc4f965cea98983cc4bb0c85882
Sean Amoroso <sean.amoroso@connectiverx.com>
2021-12-11 15:42:06 -0500

    day 12 boilahplates

---
+++ 2021/day12/day12.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Println("Day 12, Hello.")
+
+	file, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		value := scanner.Text()
+		fmt.Println(value)
+	} // end for scanner.Scan()
+}
